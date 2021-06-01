fcac9a680dc07e65a531b9d669ed698fcff252cb
Asher Oto <github@asheroto.com>
2021-06-01 02:33:15 -0500

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,106 @@
+﻿Imports Microsoft.Win32
+
+Module Module1
+    Const AppName As String = "HideUser"
+
+    Sub Main()
+
+        Dim mode As String = Nothing
+        Dim username As String = Nothing
+
+        'Get command line arguments
+        Dim s() As String = Environment.GetCommandLineArgs()
+        For i As Integer = 1 To s.Length - 1
+            Select Case LCase(s(i))
+                Case "--list"
+                    mode = "list"
+                    Exit For
+                Case "--hide"
+                    mode = "hide"
+                    username = s(i + 1)
+                    Exit For
+                Case "--show"
+                    mode = "show"
+                    username = s(i + 1)
+                    Exit For
+                Case Else
+                    ShowHelp()
+                    Exit For
+            End Select
+        Next
+
+        If mode = "" Then
+            ShowHelp()
+        ElseIf mode <> "list" And username = "" Then
+            ShowHelp()
+        End If
+
+        Console.WriteLine("")
+
+        Try
+            Dim SpecialAccountsPath As String = "SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts"
+            Dim UserListPath As String = "SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList"
+
+            Dim sa As RegistryKey = Nothing
+            Try
+                sa = Registry.LocalMachine.OpenSubKey(SpecialAccountsPath, True)
+            Catch ex As Exception
+                Registry.LocalMachine.CreateSubKey(SpecialAccountsPath)
+                sa = Registry.LocalMachine.OpenSubKey(SpecialAccountsPath, True)
+            End Try
+
+            Dim ul As RegistryKey = Nothing
+            Try
+                ul = Registry.LocalMachine.OpenSubKey(UserListPath, True)
+            Catch ex As Exception
+                Registry.LocalMachine.CreateSubKey(UserListPath)
+                ul = Registry.LocalMachine.OpenSubKey(UserListPath, True)
+            End Try
+
+            If mode = "list" Then
+                Console.WriteLine("Users hidden on logon screen:")
+                Console.WriteLine("")
+                Try
+                    For Each user As String In ul.GetValueNames()
+                        Console.WriteLine(user)
+                    Next
+                Catch ex As Exception
+                    Console.WriteLine("No users hidden")
+                End Try
+            ElseIf mode = "hide" Then
+                ul.SetValue(username, 0, RegistryValueKind.DWord)
+                Console.WriteLine(String.Format("Added ""{0}"" to users hidden on logon screen", username))
+            ElseIf mode = "show" Then
+                If ul.GetValueNames.Contains(username) Then
+                    ul.DeleteValue(username)
+                    Console.WriteLine(String.Format("Removed ""{0}"" from users hidden on logon screen", username))
+                Else
+                    Console.WriteLine(String.Format("User ""{0}"" is not hidden on logon screen", username))
+                End If
+            End If
+        Catch ex As Exception
+            Console.WriteLine("Error. Are you running as Administrator?")
+        End Try
+
+        Console.WriteLine("")
+
+    End Sub
+
+    Sub ShowHelp()
+        Console.WriteLine("")
+        Console.Write("Shows/hides user accounts from Windows logon screen.
+
+Usage: " & AppName & " [options] <Username>
+
+Options:
+    --list      Lists all users hidden from logon screen. Username argument not required here.
+    --hide      Hides user from logon screen.
+    --show      Shows user on logon screen.
+
+    --help      Shows this help
+")
+        Console.WriteLine("")
+        End
+    End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ShowHideLogin")>
+<Assembly: AssemblyDescription("Shows/hides users on Windows logon screen.")>
+<Assembly: AssemblyCompany("asher.tools")>
+<Assembly: AssemblyProduct("ShowHideLogin")>
+<Assembly: AssemblyCopyright("Copyright © 2020-2021 asher.tools")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a05abc83-3db5-4527-ab2b-78ef278a9132")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("0.0.1.0")>
+<Assembly: AssemblyFileVersion("0.0.1.0")>
