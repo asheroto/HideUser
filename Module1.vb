76165b607fd404e3e159b46738e2131855089a21
Asher Oto <github@asheroto.com>
2021-06-01 03:09:41 -0500

    Fix bug if reg keys do not exist

--- Module1.vb
+++ Module1.vb
@@ -44,35 +44,34 @@ Module Module1
         Console.WriteLine("")
 
         Try
+            Dim WinLogonPath As String = "SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
             Dim SpecialAccountsPath As String = "SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts"
             Dim UserListPath As String = "SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList"
 
-            Dim sa As RegistryKey = Nothing
-            Try
+            Dim sa As RegistryKey = Registry.LocalMachine.OpenSubKey(SpecialAccountsPath, True)
+            If sa Is Nothing Then
+                Dim wlp = Registry.LocalMachine.OpenSubKey(WinLogonPath, True)
+                wlp.CreateSubKey("SpecialAccounts")
                 sa = Registry.LocalMachine.OpenSubKey(SpecialAccountsPath, True)
-            Catch ex As Exception
-                Registry.LocalMachine.CreateSubKey(SpecialAccountsPath)
-                sa = Registry.LocalMachine.OpenSubKey(SpecialAccountsPath, True)
-            End Try
+            End If
 
-            Dim ul As RegistryKey = Nothing
-            Try
+            Dim ul As RegistryKey = Registry.LocalMachine.OpenSubKey(UserListPath, True)
+            If ul Is Nothing Then
+                sa.CreateSubKey("UserList")
                 ul = Registry.LocalMachine.OpenSubKey(UserListPath, True)
-            Catch ex As Exception
-                Registry.LocalMachine.CreateSubKey(UserListPath)
-                ul = Registry.LocalMachine.OpenSubKey(UserListPath, True)
-            End Try
+            End If
 
             If mode = "list" Then
                 Console.WriteLine("Users hidden on logon screen:")
                 Console.WriteLine("")
-                Try
+                Dim UserListValues() As String = ul.GetValueNames()
+                If UserListValues.Count = 0 Then
+                    Console.WriteLine("No users hidden")
+                Else
                     For Each user As String In ul.GetValueNames()
                         Console.WriteLine(user)
                     Next
-                Catch ex As Exception
-                    Console.WriteLine("No users hidden")
-                End Try
+                End If
             ElseIf mode = "hide" Then
                 ul.SetValue(username, 0, RegistryValueKind.DWord)
                 Console.WriteLine(String.Format("Added ""{0}"" to users hidden on logon screen", username))
@@ -85,6 +84,10 @@ Module Module1
                 End If
             End If
         Catch ex As Exception
+            Dim st As New StackTrace(True)
+            st = New StackTrace(ex, True)
+            Console.WriteLine("Line: " & st.GetFrame(0).GetFileLineNumber().ToString, "Error")
+
             Console.WriteLine("Error. Are you running as Administrator?")
         End Try
 
